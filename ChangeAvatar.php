4e593139027e2e5de3ddb149533fd0afed6c8832
Dominic Pfister <dominic.pfister@bbcag.ch>
2015-01-15 16:44:19 +0100

    Real commit (Change Avatar)
    -ChangeAvatar.css (useless)
    -ChangeAvatar.php
    -Project.css (updated)

--- ChangeAvatar.php
+++ ChangeAvatar.php
@@ -2,12 +2,23 @@
 ?>
 <link rel="stylesheet" href="ChangeAvatar.css">
 
+<!-- Code von Popup -->
 <a href="#openAvatar">Avatar &auml;ndern</a>
-
 <div id="openAvatar" class="ChangeAvatar">
 	<div>
 		<a href="#close" title="Close" class="close">X</a>
-		<h2>Avatar &auml;ndern</h2>
-		<input type="text" placeholder="Change Avatar">
+		<h2>Avatar &auml;ndern</h2><br>
+		<img src="AvatarBilder/Test.jpg" style="width:100px;height:100px"><br><br><br>
+		
+		<!-- Upload -->
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+			<div id="upload" class="upload">
+		    	<input type="file" name="fileToUpload" id="fileToUpload">
+			</div>		
+			
+			<p>Es wird empfohlen ein Bild mit mindestens 200x200 Pixel zu benutzen.</p>
+		    <input type="submit" value="Bild hochladen" name="submit">
+		</form>
 	</div>
 </div>
+
