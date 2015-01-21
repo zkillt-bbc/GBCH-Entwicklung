0413c6179853cc5a5242316d2a7a4dd25242c49a
Dominic Pfister <dominic.pfister@bbcag.ch>
2015-01-21 09:08:59 +0100

    League_of_Legends.php working

--- sites/League_of_Legends.php
+++ sites/League_of_Legends.php
@@ -1,38 +1,54 @@
 <?php
-session_start();
-include_once 'Menu_with_Banner.php';
+session_start ();
+include_once '../ressources/Menu_with_Banner.php';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>GBCH - League of Legends</title>
-<link href="Project.css" type="text/css" rel="stylesheet" />
+<link href="../css/Project.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
 	<div id="container">
-		<div class = "seite">
-			<span class = "thumbnail"><img alt="League of Legends #1 Top Laner" src="Bilder\League_of_Legends.png" width=300px height=180px></span>
-			<div class = news>
-				<text>
-					<a href="http://www.gbch.ch/games/league_of_legends">League of Legends</a></br></br>
-						<span class = "ep_name"><a href="lol_ep1.php">Episode 1: Botlane of Doom</a></span></br></br>				
-						Dies ist die erste League of legends Folge.
-						In dieser Episode spiele ich Irelia auf der Toplane und Lulu ownt auf
-						der Bot mit Lucian. </br></br></br></br></br>
-				</text>
-			</div>		
-			<span class = "thumbnail"><img alt="League of Legends #1 Top Laner" src="Bilder\League_of_Legends.png" width=300px height=180px></span>
-			<div class = news>
-				<text>
-					<a href="http://www.gbch.ch/games/league_of_legends">League of Legends</a></br></br>
-						<span class = "ep_name"><a href="Lol_ep1.php">Episode 2: Wasted!</a></span></br></br>				
-						Dies ist die erste League of legends Folge.
-						In dieser Episode spiele ich Irelia auf der Toplane und Lulu ownt auf
-						der Bot mit Lucian. </br></br></br></br></br>
-				</text>
-			</div>		
+		<div class="seite">
+
+			<!-- Episode 1 -->
+			<span class="thumbnail"><img
+				src="../ressources/Bilder/League_of_Legends.png" width=300px
+				height=180px></span>
+			<div class=news>
+				<a href="http://www.gbch.ch/games/league_of_legends">League of
+					Legends</a><br>
+				<br> <span class="ep_name"><a href="lol_ep1.php">Episode 1: Botlane of Doom</a></span><br>
+				<br> Dies ist die erste League of legends Folge. In dieser Episode
+				spiele ich Irelia auf der Toplane und Lulu ownt auf der Bot mit
+				Lucian. <br>
+				<br>
+				<br>
+				<br>
+				<br>
+			</div>
+
+			<!-- Episode 2 -->
+			<span class="thumbnail"><img
+				src="../ressources/Bilder/League_of_Legends.png" width=300px
+				height=180px></span>
+			<div class=news>
+				<a href="http://www.gbch.ch/games/league_of_legends">League of
+					Legends</a><br>
+				<br> <span class="ep_name"><a href="Lol_ep1.php">Episode 2: Wasted!</a></span><br>
+				<br> Sample text <br>
+				<br>
+				<br>
+				<br>
+				<br>
+			</div>
+		
+		<!-- end tag for #seite -->
 		</div>
+	
+	<!-- end tag for #container -->
 	</div>
 </body>
 </html>
