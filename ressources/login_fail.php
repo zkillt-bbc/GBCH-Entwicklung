2d147826e7128f84a0a63ae18e3dacdc9dd5a933
Tim Killenberger <Tim.Killenberger@bbcag.ch>
2015-01-21 11:07:39 +0100

    Login Fail fixed and working now

--- ressources/login_fail.php
+++ ressources/login_fail.php
@@ -7,7 +7,7 @@ require_once 'Menu_with_Banner.php';
 <head>
 <meta charset="UTF-8">
 <title>GBCH - Kontakt</title>
-<link href="Project.css" type="text/css" rel="stylesheet" />
+<link href="../css/Project.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
 	<div id="container">
@@ -18,13 +18,13 @@ require_once 'Menu_with_Banner.php';
 				</div>
 			<div class = formular>
 				<form action="eintragen.php" method="post">
-					<input id="Username" name="Username" placeholder="Username" required/></br></br>
-					<input id="Name" name="Name" placeholder="Nachname" required/></br></br>
-					<input id="Vorname" name="Vorname" placeholder="Vorname" required/></br></br>
-					<input id="Email" name="Email" type="email" placeholder="E-Mail" required/></br></br>
-					<input id="Passwort" name="Passwort" type="password" placeholder="Passwort" required/></br></br>
-					<input id="Passwort_bestätigen" name="Passwort_bestätigen" type="password" placeholder="Passwort best&auml;tigen" required/></br></br>
-					<input id ="Geburtsdatum" name="Geburtsdatum" type="date" reguired></br></br>
+					<input id="Username" name="Username" placeholder="Username" required/><br><br>
+					<input id="Name" name="Name" placeholder="Nachname" required/><br><br>
+					<input id="Vorname" name="Vorname" placeholder="Vorname" required/><br><br>
+					<input id="Email" name="Email" type="email" placeholder="E-Mail" required/><br><br>
+					<input id="Passwort" name="Passwort" type="password" placeholder="Passwort" required/><br><br>
+					<input id="Passwort_bestätigen" name="Passwort_bestätigen" type="password" placeholder="Passwort best&auml;tigen" required/><br><br>
+					<input id ="Geburtsdatum" name="Geburtsdatum" type="date" required><br><br>
 					<input id ="submit" type="submit" name="submit" value="Registrieren"/>
 				</form>
 			</div>
@@ -35,8 +35,8 @@ require_once 'Menu_with_Banner.php';
 			</div>
 			<div class = formular>
 				<form action="login1.php" method="post">
-					<input id="Username" name="Username" placeholder="Username" required/></br></br>
-					<input id="Passwort" name="Passwort" type="password" placeholder="Passwort" reguired/></br></br>		
+					<input id="Username" name="Username" placeholder="Username" required/><br><br>
+					<input id="Passwort" name="Passwort" type="password" placeholder="Passwort" required/><br><br>		
 					<input type="submit" value="Login"/>
 					<span class = "login_failed"><p>Login Fehlgeschlagen!</p></span>
 				</form>
